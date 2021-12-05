092ab18abf2e3b3efab0811ced7e74ff042271da
Gelson Nascimento <gelsonschikorski@hotmail.com>
2021-12-05 15:45:14 -0300

    S02-02 pronto

--- S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -18,7 +18,11 @@ package com.example.android.sunshine;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.utilities.NetworkUtils;
@@ -102,7 +106,28 @@ public class MainActivity extends AppCompatActivity {
     // TODO (4) Set the title of the menu item to "Refresh" using strings.xml
 
     // TODO (5) Override onCreateOptionsMenu to inflate the menu for this Activity
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.forecast, menu);
+
+        return true;
+    }
+
     // TODO (6) Return true to display the menu
 
     // TODO (7) Override onOptionsItemSelected to handle clicks on the refresh button
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_refresh){
+            mWeatherTextView.setText("");
+            loadWeatherData();
+            Toast.makeText(this, "Clicked on the button", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
